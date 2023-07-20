d9d0c50169b1effa0cbb3fc620b005ae9c5b6f3b
Bruno Barbosa <75800617+JBrunoBarbosa@users.noreply.github.com>
2023-07-19 23:35:39 -0300

    Update api.py

--- api.py
+++ api.py
@@ -4,13 +4,11 @@ import tabula
 import pandas as pd
 import re
 import time
-import threading
 
 app = Flask(__name__)
 CORS(app)
 
 tabela_horarios_vagos = None
-lock = threading.Lock()
 
 @app.route('/')
 def index():
@@ -44,36 +42,28 @@ def processar_arquivo(arquivo):
             if not horario_vago:
                 horarios_vagos.append((dia, horario))
 
-    with lock:
-        global tabela_horarios_vagos
-        if tabela_horarios_vagos is None:
-            tabela_horarios_vagos = set(horarios_vagos)
-        else:
-            tabela_horarios_vagos = tabela_horarios_vagos.intersection(set(horarios_vagos))
+    if tabela_horarios_vagos is None:
+        tabela_horarios_vagos = set(horarios_vagos)
+    else:
+        tabela_horarios_vagos = tabela_horarios_vagos.intersection(set(horarios_vagos))
 
-        dados_aluno = {
-            'Nome': nome_aluno,
-            'Curso': curso_aluno
-        }
+    dados_aluno = {
+        'Nome': nome_aluno,
+        'Curso': curso_aluno
+    }
 
-        # Retorna o aluno e seus horários vagos
-        return dados_aluno
+    # Retorna o aluno e seus horários vagos
+    return dados_aluno
 
 @app.route('/api/horarios-vagos', methods=['POST'])
 def horarios_vagos():
     start_time = time.time()
 
     arquivos = request.files.getlist('file')
-    threads = []
     dados_alunos = []
 
     for arquivo in arquivos:
-        thread = threading.Thread(target=lambda: dados_alunos.append(processar_arquivo(arquivo)))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
+        dados_alunos.append(processar_arquivo(arquivo))
 
     # Cria o DataFrame para exibir a tabela
     df_tabela = pd.DataFrame(list(tabela_horarios_vagos), columns=['Dia', 'Horario'])
@@ -92,5 +82,4 @@ def horarios_vagos():
     return resp
 
 if __name__ == '__main__':
-    app.debug = True
     app.run(host='0.0.0.0', port=5000)
